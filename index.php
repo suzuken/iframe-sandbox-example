454b28b4f6266d05846e47b5ab764c934c859920
Kenta SUZUKI <suzuken326@gmail.com>
2017-06-01 13:12:37 +0900

    example: read top.location

--- index.php
+++ index.php
@@ -13,10 +13,19 @@ header("X-Frame-Options: DENY");
 <p>sandbox: child.html</p>
 <p>child.html calls frame.html which uses top.location</p>
 <iframe src="http://localhost:8888/child.html" sandbox="allow-scripts allow-forms allow-pointer-lock allow-popups allow-same-origin" frameborder="0" width="0" height="0"></iframe>
+<br>
 This cause output something like:
 <br>
 
 > Unsafe JavaScript attempt to initiate navigation for frame with URL 'http://localhost:8888/' from frame with URL 'http://localhost:8888/frame.html'. The frame attempting navigation of the top-level window is sandboxed, but the flag of 'allow-top-navigation' or 'allow-top-navigation-by-user-activation' is not set.
+
+<h4>read top.locaton properties</h4>
+
+<p>in iframe sandbox</p>
+<iframe src="http://localhost:8888/readtop.html" sandbox="allow-scripts allow-forms allow-pointer-lock allow-popups allow-same-origin" frameborder="0"></iframe>
+
+<p>normal iframe</p>
+<iframe src="http://localhost:8888/readtop.html" frameborder="0"></iframe>
 
 <hr>
 
